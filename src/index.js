a64256ae3074046b49e2b0485965ebe78b68945f
Joaquin <138231434+joaquinrodriguezpou@users.noreply.github.com>
2023-11-24 19:31:00 -0300

    create nav buttons

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { loadMenuPage } from './menu.js';
+
+const container = document.createElement('div');
+container.id = 'container';
+
+const navBtns = document.querySelector('.nav-btns');
+const homeBtn = document.createElement('button');
+const menuBtn = document.createElement('button');
+const contactBtn = document.createElement('button');
+
+homeBtn.textContent = 'Home';
+menuBtn.textContent = 'Menu';
+contactBtn.textContent = 'Contact';
+navBtns.appendChild(homeBtn);
+navBtns.appendChild(menuBtn);
+navBtns.appendChild(contactBtn);
